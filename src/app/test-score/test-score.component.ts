46418edd37a207e39bf667ca879a90ee5dd29f8b
unknown <trandrew@mail.fresnostate.edu>
2018-12-20 17:36:03 -0800

    Done

--- src/app/test-score/test-score.component.ts
+++ src/app/test-score/test-score.component.ts
@@ -3,6 +3,16 @@ import { Http } from '@angular/http';
 import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';
 import { ToastService } from '../toast/toast.service';
+import { isDefaultChangeDetectionStrategy } from '@angular/core/src/change_detection/constants';
+
+export interface ITest{
+  id: number;
+  testName: string;
+    pointsPossible: number;
+    pointsReceived: number;
+    percentage: number;
+    grade: string;
+}
 
 
 @Component({
@@ -21,7 +31,45 @@ export class TestScoreComponent implements OnInit {
   ) { }
 
   async ngOnInit() {
-
+    const savedTests = JSON.parse(localStorage.getItem('tests'));
+    if(savedTests && savedTests.length > 0){
+      this.tests = savedTests;
+    } else{
+      this.tests = await this.loadTestFromFile();
+    }
+    
+  
   }
 
+async loadTestFromFile() {
+ const tests = await this.http.get('assets/tests.json').toPromise();
+ console.log('test-->', tests.json());
+ return tests.json();
+}
+
+//ADDING TEST FUNCTION
+addTest() {
+  const newTest: ITest = {
+    'id': null,
+    'testName': null,
+    'pointsPossible': null,
+    'pointsReceived': null,
+    'percentage': null,
+    'grade': null
+  };
+  this.tests.unshift(newTest); 
+//LOCAL STORAGE stuff
+  this.saveToLocalStorage('tests', this.tests)
+
+  }
+  saveToLocalStorage(key: string, data) {
+    localStorage.setItem(key,JSON.stringify(data));
+
+    
+  }
+  //Delete Test function
+  deleteTest(index:number){
+    this.tests.splice(index, 1);
+    this.saveToLocalStorage('test', this.tests);
+  }
 }
